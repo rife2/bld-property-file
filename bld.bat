10c5dc73480166b3cb61bf028d5eaf91f3490660
Erik C. Thauvin <erik@thauvin.net>
2023-04-03 02:20:09 -0700

    Updated to RIFE2 1.5.14

--- bld.bat
+++ bld.bat
@@ -1,4 +1,4 @@
 @echo off
 set DIRNAME=%~dp0
 if "%DIRNAME%" == "" set DIRNAME=.
-java -jar "%DIRNAME%/lib/bld/bld-wrapper.jar" "%0" --build rife.bld.extension.propertyFile.PropertyFileBuild %*
+java -jar "%DIRNAME%/lib/bld/bld-wrapper.jar" "%0" --build rife.bld.extension.propertyfile.PropertyFileBuild %*
