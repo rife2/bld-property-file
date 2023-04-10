0e89117b7fb7759180b8f6963eac3362e0ca0310
Geert Bevin <gbevin@uwyn.com>
2023-04-10 08:19:17 -0400

    Updated to RIFE2 1.5.19

--- src/bld/java/rife/bld/extension/propertyfile/PropertyFileBuild.java
+++ src/bld/java/rife/bld/extension/propertyfile/PropertyFileBuild.java
@@ -18,40 +18,44 @@ public class PropertyFileBuild extends Project {
         pkg = "rife.bld.extension";
         name = "bld-property-file";
         version = version(0, 9, 1);
-        javadocOptions
-                .docLint(NO_MISSING)
-                .link("https://rife2.github.io/rife2/");
-        publishRepository = version.isSnapshot() ? repository("rife2-snapshot") : repository("rife2");
-//        publishRepository = MAVEN_LOCAL;
-        publishInfo = new PublishInfo()
-                .groupId("com.uwyn.rife2")
-                .artifactId("bld-property-file")
-                .description("bld Extension to Create or Modify Properties Files")
-                .url("https://github.com/rife2/bld-property-file")
-                .developer(new PublishDeveloper().id("ethauvin").name("Erik C. Thauvin").email("erik@thauvin.net")
-                        .url("https://erik.thauvin.net/"))
-                .developer(new PublishDeveloper().id("gbevin").name("Geert Bevin").email("gbevin@uwyn.com")
-                        .url("https://github.com/gbevin"))
-                .license(new PublishLicense().name("The Apache License, Version 2.0")
-                        .url("http://www.apache.org/licenses/LICENSE-2.0.txt"))
-                .scm(new PublishScm().connection("scm:git:https://github.com/rife2/bld-property-file.git")
-                        .developerConnection("scm:git:git@github.com:rife2/bld-property-file.git")
-                        .url("https://github.com/rife2/bld-property-file"))
-                .signKey(property("sign.key"))
-                .signPassphrase(property("sign.passphrase"));
 
         javaRelease = 17;
         downloadSources = true;
         autoDownloadPurge = true;
-        repositories = List.of(MAVEN_CENTRAL, RIFE2);
+        repositories = List.of(MAVEN_CENTRAL, RIFE2_RELEASES);
 
         scope(compile)
-                .include(dependency("com.uwyn.rife2", "rife2", version(1, 5, 17)));
+                .include(dependency("com.uwyn.rife2", "rife2", version(1, 5, 19)));
         scope(test)
                 .include(dependency("org.jsoup", "jsoup", version(1, 15, 4)))
                 .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 9, 2)))
                 .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 9, 2)))
                 .include(dependency("org.assertj:assertj-joda-time:2.2.0"));
+
+        javadocOperation()
+            .javadocOptions()
+                .docLint(NO_MISSING)
+                .link("https://rife2.github.io/rife2/");
+
+        publishOperation()
+            .repository(version.isSnapshot() ? repository("rife2-snapshot") : repository("rife2"))
+//            .repository(MAVEN_LOCAL)
+            .info()
+                .groupId("com.uwyn.rife2")
+                .artifactId("bld-property-file")
+                .description("bld Extension to Create or Modify Properties Files")
+                .url("https://github.com/rife2/bld-property-file")
+                .developer(new PublishDeveloper().id("ethauvin").name("Erik C. Thauvin").email("erik@thauvin.net")
+                    .url("https://erik.thauvin.net/"))
+                .developer(new PublishDeveloper().id("gbevin").name("Geert Bevin").email("gbevin@uwyn.com")
+                    .url("https://github.com/gbevin"))
+                .license(new PublishLicense().name("The Apache License, Version 2.0")
+                    .url("http://www.apache.org/licenses/LICENSE-2.0.txt"))
+                .scm(new PublishScm().connection("scm:git:https://github.com/rife2/bld-property-file.git")
+                    .developerConnection("scm:git:git@github.com:rife2/bld-property-file.git")
+                    .url("https://github.com/rife2/bld-property-file"))
+                .signKey(property("sign.key"))
+                .signPassphrase(property("sign.passphrase"));
     }
 
     public static void main(String[] args) {
