accae48297a4bdfbe9a7a1d30f17917144da992d
Erik C. Thauvin <erik@thauvin.net>
2024-01-30 23:52:04 -0800

    Version 0.9.4

--- src/bld/java/rife/bld/extension/propertyfile/PropertyFileBuild.java
+++ src/bld/java/rife/bld/extension/propertyfile/PropertyFileBuild.java
@@ -37,7 +37,7 @@ public class PropertyFileBuild extends Project {
     public PropertyFileBuild() {
         pkg = "rife.bld.extension";
         name = "bld-property-file";
-        version = version(0, 9, 4, "SNAPSHOT");
+        version = version(0, 9, 4);
 
         javaRelease = 17;
         downloadSources = true;
