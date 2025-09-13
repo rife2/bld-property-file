125ad41a243d392c094e26ed58f96fc5a4303f22
Erik C. Thauvin <erik@thauvin.net>
2025-09-12 18:13:40 -0700

    Bump JSoup to version 1.21.2

--- src/bld/java/rife/bld/extension/propertyfile/PropertyFileBuild.java
+++ src/bld/java/rife/bld/extension/propertyfile/PropertyFileBuild.java
@@ -53,9 +53,9 @@ public class PropertyFileBuild extends Project {
         scope(compile)
                 .include(dependency("com.uwyn.rife2", "bld", version(2, 3, 0)));
         scope(test)
-                .include(dependency("org.jsoup", "jsoup", version(1, 21, 1)))
                 .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 13, 3)))
                 .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 13, 3)))
+                .include(dependency("org.jsoup", "jsoup", version(1, 21, 2)))
                 .include(dependency("org.assertj:assertj-joda-time:2.2.0"));
 
         javadocOperation()
