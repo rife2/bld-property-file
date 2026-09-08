898459d288aedff0faa8bad2c475987292effde6
Erik C. Thauvin <erik@thauvin.net>
2026-09-08 13:30:11 -0700

    Bump Testing Helpers to version 1.1.0

--- src/bld/java/rife/bld/extension/propertyfile/PropertyFileBuild.java
+++ src/bld/java/rife/bld/extension/propertyfile/PropertyFileBuild.java
@@ -61,7 +61,7 @@ public class PropertyFileBuild extends Project {
                         version(4, 10, 4)));
         scope(test)
                 .include(dependency("com.uwyn.rife2", "bld-testing-helpers",
-                        version(1, 1, 0, "SNAPSHOT")))
+                        version(1, 1, 0)))
                 .include(dependency("org.jsoup", "jsoup", version(1, 23, 2)))
                 .include(dependency("org.junit.jupiter", "junit-jupiter", junit))
                 .include(dependency("org.junit.platform", "junit-platform-console-standalone", junit))
